fa23893dee5d91a0b7571c25cc561a943096ae96
Florian Delage <delageflo@ymail.com>
2021-12-23 23:54:02 +0100

    create Profile model

--- bballstats/settings.py
+++ bballstats/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Redirections (login, logout)
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
