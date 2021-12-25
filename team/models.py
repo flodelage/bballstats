a02b08036e825ab34c08b8109c6e801fb2fee551
Florian Delage <delageflo@ymail.com>
2021-12-25 18:09:00 +0100

    create Team model

--- team/models.py
+++ team/models.py
@@ -1,3 +1,32 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Team(models.Model):
+    name = models.CharField(
+        'Nom du club',
+        max_length=128
+    )
+    city = models.CharField(
+        "Ville",
+        max_length=128
+    )
+    province = models.CharField(
+        'Département',
+        max_length=128
+    )
+    level = models.CharField(
+        'Niveau',
+        max_length=128
+    )
+    picture = models.ImageField(
+        'Photo',
+        null=True
+    )
+
+    # Foreign Key(s):
+    profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
