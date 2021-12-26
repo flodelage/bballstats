b349dd8d78e1c0b8df06d53c5fe89b638f7d85d8
Florian Delage <delageflo@ymail.com>
2021-12-26 17:46:18 +0100

    user can display team's games list

--- game/views.py
+++ game/views.py
@@ -1,7 +1,14 @@
 
 from django.shortcuts import render
 
+from .models import Game
+
 
 def games_list(request, username, team_pk):
     username = request.user.username
-    games = Game.objects.filter()
+    games = Game.objects.filter(team__pk=team_pk)
+    return render(
+        request,
+        'game/games-list.html',
+        {'username': username, 'games': games}
+    )
