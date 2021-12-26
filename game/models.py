9d2a0e31e48799b258a8bb9efafb6b160fecdbc7
Florian Delage <delageflo@ymail.com>
2021-12-26 16:15:20 +0100

    create games fixtures

--- game/models.py
+++ game/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 
 class Game(models.Model):
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
     opponent = models.CharField(
         max_length=128
     )
-    final_score = models.CharField(
-        max_length=128
+    opponent_points = models.PositiveSmallIntegerField(
+        verbose_name="Points de l'adversaire",
+        default=0
     )
     comment = models.TextField(null=True)
