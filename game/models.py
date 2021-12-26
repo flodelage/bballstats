977db552b26c55817a1883abd81c40158ff3e3d4
Florian Delage <delageflo@ymail.com>
2021-12-26 15:32:14 +0100

    create Game model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    date = models.DateField()
+    opponent = models.CharField(
+        max_length=128
+    )
+    final_score = models.CharField(
+        max_length=128
+    )
+    comment = models.TextField(null=True)
