048ef05bf91cf1b7381bbeed86ac0fbdf0d5a58d
Florian Delage <delageflo@ymail.com>
2021-12-26 18:03:40 +0100

    user can display game detail

--- game/urls.py
+++ game/urls.py
@@ -6,4 +6,5 @@ from game import views
 # urls start with: account/<str:username>/team/<str:team_pk>/games/
 urlpatterns = [
     path('', views.games_list, name='games_list'),
+    path('<int:game_pk>/', views.game_detail, name='game_detail'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,14 +1,25 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Game
+from team.models import Team
 
 
 def games_list(request, username, team_pk):
     username = request.user.username
+    team = get_object_or_404(Team, pk=team_pk)
     games = Game.objects.filter(team__pk=team_pk)
     return render(
         request,
         'game/games-list.html',
-        {'username': username, 'games': games}
+        {'username': username, 'team': team, 'games': games}
+    )
+
+def game_detail(request, username, team_pk, game_pk):
+    username = request.user.username
+    game = get_object_or_404(Game, pk=game_pk)
+    return render(
+        request,
+        'game/game-detail.html',
+        {'username': username, 'game': game,}
     )
