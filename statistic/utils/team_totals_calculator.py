c70e45e97c68ce71537bfbfedcd670500f13203d
Florian Delage <delageflo@ymail.com>
2021-12-27 23:12:21 +0100

    cancel last commit

--- statistic/utils/team_totals_calculator.py
+++ statistic/utils/team_totals_calculator.py
@@ -21,7 +21,7 @@ class TeamTotalsCalculator():
         }
 
 
-    def calculate_team_statistics(self, players_statistics):
+    def add_players_statistics(self, players_statistics):
         """
         Calculate and return team statistics for a game
         by adding all players stats
@@ -42,4 +42,45 @@ class TeamTotalsCalculator():
             self.team_stats['blocks'] = stats.blocks + self.team_stats['blocks']
             self.team_stats['turnovers'] = stats.turnovers + self.team_stats['turnovers']
             self.team_stats['fouls'] = stats.fouls + self.team_stats['fouls']
+        return self.team_stats
+
+
+    def field_goals_percent(self):
+        """
+        Calculate field goal percent from
+        3pts and 2pts shots
+        """
+        try:
+            self.team_stats['field_goals_percent'] = round((self.team_stats['field_goals_made']/self.team_stats['field_goals_attempted']) *100, 1)
+        except ZeroDivisionError:
+            self.team_stats['field_goals_percent'] = 0.0
+
+
+    def three_points_percent(self):
+        """
+        Calculate field goal percent from
+        3pts shots
+        """
+        try:
+            self.team_stats['three_points_percent'] = round((self.team_stats['three_points_made']/self.team_stats['three_points_attempted']) *100, 1)
+        except ZeroDivisionError:
+            self.team_stats['three_points_percent'] = 0.0
+
+
+    def free_throws_percent(self):
+        """
+        Calculate field goal percent from
+        free throws shots
+        """
+        try:
+            self.team_stats['free_throws_percent'] = round((self.team_stats['free_throws_made']/self.team_stats['free_throws_attempted']) *100, 1)
+        except ZeroDivisionError:
+            self.team_stats['free_throws_percent'] = 0.0
+
+
+    def teams_statistics(self, players_statistics):
+        self.add_players_statistics(players_statistics)
+        self.field_goals_percent()
+        self.three_points_percent()
+        self.free_throws_percent()
         return self.team_stats
